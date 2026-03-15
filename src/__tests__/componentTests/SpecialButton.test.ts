aca4ba1f3627881a5c3a5215a7699c73fd376e75
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-03-15 16:21:32 +0100

    implement tests for component based testing

--- src/__tests__/componentTests/SpecialButton.test.ts
+++ src/__tests__/componentTests/SpecialButton.test.ts
@@ -1,1 +1,25 @@
-import {}
+import { mount } from '@vue/test-utils'
+
+import SpecialButton from '../../components/SpecialButton.vue'
+
+describe('SpecialButton.vue', () => {
+  it('renders label correctly', () => {
+    const wrapper = mount(SpecialButton, {
+      props: {
+        label: 'Click me',
+      },
+    })
+
+    expect(wrapper.text()).toContain('Click me')
+  })
+
+  it('emits proper click event', async () => {
+    const wrapper = mount(SpecialButton, {
+      props: { label: 'Click' },
+    })
+
+    await wrapper.trigger('click')
+
+    expect(wrapper.emitted()).toHaveProperty('clicked')
+  })
+})
