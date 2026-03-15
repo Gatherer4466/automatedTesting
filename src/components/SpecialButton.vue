1d92619869e46b4a7a05aa618d9a3b032a58a2b8
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-03-15 16:15:10 +0100

    define props and emites

--- src/components/SpecialButton.vue
+++ src/components/SpecialButton.vue
@@ -2,4 +2,14 @@
   <button></button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface Props {
+  label: string
+}
+
+const props = defineProps<Props>()
+
+const emit = defineEmits<{
+  (e: 'clicked'): void
+}>()
+</script>
