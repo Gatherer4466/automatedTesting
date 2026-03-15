796ad028e264a1f07d1a43d9a0916c5cd04264b9
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-03-15 16:55:47 +0100

    try to fix site just not working

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
+<template>
+  <div id="app">
+    <img src="https://cdn.7tv.app/emote/01GTS8BZ9R000D50ZC809HM347/4x.png" />
+  </div>
+</template>
+
 <script setup lang="ts">
 window.location.href = 'https://github.com/Gatherer4466/automatedTesting/'
 </script>
-
-<template>
-  <img src="https://cdn.7tv.app/emote/01GTS8BZ9R000D50ZC809HM347/4x.png" />
-</template>
 
 <style scoped></style>
